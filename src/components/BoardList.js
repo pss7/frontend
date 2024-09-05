b3f1cc0078e8e9aa0c5b1759e1333bb242ef8eb9
parksangseok <>
2024-09-05 17:55:40 +0900

    commit

--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -11,12 +11,7 @@ export default function BoardList() {
     const offset = (page - 1) * limit;
 
     useEffect(() => {
-        fetch(`${process.env.REACT_APP_API}/db/db.json`, {
-            headers: {
-                Accept: "application / json",
-            },
-            method: "GET",
-        })
+        fetch("http://localhost:3001/board//board")
             .then(res =>
                 res.json()
             )
